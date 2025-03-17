101a5287b7ee0dbe1e4c5ad5b72cad0c1eb41eab
Ivans Mihailovs <imi@mediaan.com>
2025-03-17 09:44:32 +0100

    feat: implement theming
    Refs: #2

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
 final ThemeData critterCompanionTheme = ThemeData(
-  primarySwatch: Colors.blue,
+  // Color set for this app
+  // primary 4B8887 (teal)
+  // accent 02233D (dark blue)
+  // alert BC4749 (tomato)
+  // alert light E6AACE (light pink)
+  // light background EFEEDA (pale lime)
+  primaryColor: const Color(0xFF4B8887),
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color(0xFF4B8887),
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 24,
+    ),
+  ),
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    selectedItemColor: Color(0xFF4B8887),
+    unselectedItemColor: Colors.grey,
+  ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      foregroundColor: const Color(0xFF4B8887),
+      side: const BorderSide(width: 2, color: Color(0xFF4B8887)),
+    ),
+  ),
+  filledButtonTheme: FilledButtonThemeData(
+      style: FilledButton.styleFrom(
+    backgroundColor: const Color(0xFF02233D),
+  )),
 );
